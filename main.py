b20f0b706541d98530f3dbdc21d989a35a2494c3
Kelsey Rough <kelsey.rough.379@my.werbr.ca>
2021-10-27 10:39:29 -0400

    Updated/simplified comments

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import cv2
-# Load the cascade
+count = 0
+# Load the cascade (For this workshop we're using a premade Haar cascade)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 img = cv2.imread('people.jpeg')
-# Convert into grayscale
+# Convert the image into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detect faces using the face cascade
+# Detect the faces using the face cascade
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    count = count+1
 # Display the output (change to save image results later)
 cv2.imshow('img', img)
 cv2.waitKey(0)
-#cv2.imwrite("cartoon.jpg", output)
+print("There are " + count + "faces")
