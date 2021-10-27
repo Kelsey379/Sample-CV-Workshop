b18e4a99658c77ec0119fdf2f216677caa4ff8f8
Kelsey Rough <kelsey.rough.379@my.werbr.ca>
2021-10-27 10:32:19 -0400

    added ideas for later changes

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('people.jpeg')
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detect faces
+# Detect faces using the face cascade
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-# Display the output
+# Display the output (change to save image results later)
 cv2.imshow('img', img)
 cv2.waitKey(0)
 #cv2.imwrite("cartoon.jpg", output)
